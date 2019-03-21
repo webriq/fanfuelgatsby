daf375d07bf22f17d6befd05ba9f8af174094c1a
Dorell James Galang <galangdj@gmail.com>
2019-03-21 17:06:11 +0800

    Fix sanity client import

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { createMarkup } from "../helpers"
-import { sanityClient } from "@sanity/client"
+import sanityClient from "@sanity/client"
 
 class PreviewPage extends React.Component {
   constructor(props) {
