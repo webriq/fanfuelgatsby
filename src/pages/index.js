a4c9581d091e8667d83626402c42739564f6cd17
Dorell James Galang <galangdj@gmail.com>
2019-03-21 16:31:42 +0800

    Enable previews

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import { createSlug, printObj, mapEdgesToNodes, createMarkup } from "../helpers"
+import { createSlug, mapEdgesToNodes, createMarkup } from "../helpers"
 
 class IndexPage extends React.Component {
   render() {
---
+++ src/pages/preview.js
@@ -0,0 +1,65 @@
+import React from "react"
+import Layout from "../components/layout"
+import { createMarkup } from "../helpers"
+const sanityClient = require("@sanity/client")
+const client = sanityClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  token: process.env.SANITY_API_READ_TOKEN, // or leave blank to be anonymous user
+  useCdn: false, // `false` if you want to ensure fresh data
+})
+
+class PreviewPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      post: null,
+    }
+  }
+
+  componentDidMount() {
+    const query = `*[_type == 'post' && _id == $id]`
+    const currentPostId = this.props.location.search.split("=")[1]
+    const params = { id: currentPostId }
+
+    client
+      .fetch(query, params)
+      .then(post => {
+        this.setState({
+          isLoaded: true,
+          post: post[0],
+        })
+      })
+      .catch(error => {
+        this.setState({
+          isLoaded: true,
+          error,
+        })
+      })
+  }
+
+  render() {
+    const { error, isLoaded, post } = this.state
+
+    if (error) {
+      return <Layout>Error: {error.message}</Layout>
+    } else if (!isLoaded) {
+      return (
+        <Layout>
+          <p>Fetching data...</p>
+        </Layout>
+      )
+    } else {
+      return (
+        <Layout>
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={createMarkup(post.body)} />
+        </Layout>
+      )
+    }
+  }
+}
+
+export default PreviewPage
